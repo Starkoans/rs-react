0a1b8dd7d9df56d61d6157effb9b0d78cbefc301
Anastasiia <starkoans@gmail.com>
2025-07-18 10:05:36 +0500

    refactor: error boundary test

--- src/tests/error-boundary.test.tsx
+++ src/tests/error-boundary.test.tsx
@@ -3,24 +3,24 @@ import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { ErrorBoundary } from '../components/error-boundary/error-boundary';
 import { messages } from '../sources/messages';
-import { Component } from 'react';
-
-class ProblemChild extends Component {
-  componentDidMount(): void {
-    throw new Error();
-  }
-}
+import { SimulateError } from '../components/simulate-error';
+import userEvent from '@testing-library/user-event';
 
 describe('Error boundary', () => {
-  it('should catch error and show fallback UI', () => {
+  it('should catch error and show fallback UI when user clicked simulate error button', async () => {
     render(
       <ErrorBoundary>
-        <ProblemChild />
+        <SimulateError />
       </ErrorBoundary>
     );
 
-    expect(screen.getByText(messages.errors.oops)).toBeInTheDocument();
+    const user = userEvent.setup();
 
+    await user.click(
+      screen.getByRole('button', { name: messages.buttons.simulateError })
+    );
+
+    expect(screen.getByText(messages.errors.oops)).toBeInTheDocument();
     expect(
       screen.getByRole('button', { name: messages.buttons.reload })
     ).toBeInTheDocument();
