81ba97b31a768356f35f5c1d85f7f30ea70cd3be
Anastasiia <starkoans@gmail.com>
2025-07-18 09:58:21 +0500

    feat: test for the search input to make record in local storage

--- src/tests/search.test.tsx
+++ src/tests/search.test.tsx
@@ -1,7 +1,10 @@
 import '@testing-library/jest-dom/vitest';
+
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Search } from '../components/search/search';
+import { LSKeys } from '../sources/ls-keys';
 
 describe('search', () => {
   const setup = () => {
@@ -19,5 +22,16 @@ describe('search', () => {
     const button = screen.getByRole('button', { name: /search/i });
     expect(button).toBeInTheDocument();
     expect(button).toBeInstanceOf(HTMLButtonElement);
+  });
+
+  it('writes value to localStorage after input change', async () => {
+    localStorage.clear();
+    const input = screen.getByPlaceholderText(/search/i);
+    const user = userEvent.setup();
+
+    const searchTerm = 'cornish';
+    await user.type(input, searchTerm);
+
+    expect(localStorage.getItem(LSKeys.SearchInput)).toBe(searchTerm);
   });
 });
