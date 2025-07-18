251e4400884c8762513fd29fabc945213132f3b4
Anastasiia <starkoans@gmail.com>
2025-07-18 15:48:57 +0500

    chore: aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    setupFiles: ['./src/tests/setup-tests.ts'],
+    setupFiles: ['./src/__tests__/setup-tests.ts'],
+    globals: true,
     environment: 'jsdom',
     coverage: {
       provider: 'v8',
@@ -23,6 +25,14 @@ export default defineConfig({
         functions: 50,
         lines: 50,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@sources': path.resolve(__dirname, 'src/sources'),
+      '@tests': path.resolve(__dirname, 'src/__tests__'),
     },
   },
 });
