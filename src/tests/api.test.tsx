60691d1085b82456af347a13350bcfab8ef20a3e
Anastasiia <starkoans@gmail.com>
2025-07-18 10:46:23 +0500

    feat: test for cat card image query

--- src/tests/api.test.tsx
+++ src/tests/api.test.tsx
@@ -4,7 +4,6 @@ import { afterEach, describe, expect, it, vi } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as api from '../api/fetch-cats-breed';
-import { fetchCats } from '../api/fetch-cats-breed';
 import App from '../App';
 import { mockCats } from './mock-cats';
 import { messages } from '../sources/messages';
@@ -13,7 +12,7 @@ vi.mock('../api/fetch-cats-breed', () => ({
   fetchCats: vi.fn(),
 }));
 
-describe('search', () => {
+describe('sapi integration', () => {
   afterEach(() => {
     cleanup();
     localStorage.clear();
@@ -28,7 +27,7 @@ describe('search', () => {
     await userEvent.type(input, 'british');
     await userEvent.click(button);
 
-    expect(fetchCats).toHaveBeenCalledWith('british');
+    expect(api.fetchCats).toHaveBeenCalledWith('british');
     expect(await screen.findByText(/British Shorthair/i)).toBeInTheDocument();
   });
 
@@ -41,12 +40,12 @@ describe('search', () => {
     await userEvent.type(input, 'unknown cat');
     await userEvent.click(button);
 
-    expect(fetchCats).toHaveBeenCalledWith('unknown cat');
+    expect(api.fetchCats).toHaveBeenCalledWith('unknown cat');
     expect(await screen.findByText(messages.noCatsFound)).toBeInTheDocument();
   });
 
   it('handles fetch error', async () => {
-    (fetchCats as ReturnType<typeof vi.fn>).mockRejectedValue({
+    (api.fetchCats as ReturnType<typeof vi.fn>).mockRejectedValue({
       message: 'Request failed with status code 404',
       response: { status: 404 },
     });
@@ -58,7 +57,7 @@ describe('search', () => {
     await userEvent.type(input, 'nonexistentbreed');
     await userEvent.click(button);
 
-    expect(fetchCats).toHaveBeenCalled();
+    expect(api.fetchCats).toHaveBeenCalled();
     expect(await screen.findByText(messages.errors.oops)).toBeInTheDocument();
   });
 });
--- src/tests/cat-card.test.tsx
+++ src/tests/cat-card.test.tsx
@@ -1,14 +1,62 @@
 import '@testing-library/jest-dom/vitest';
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
 import { CatCard } from '../components/cat-card/cat-card';
 import { mockCats } from './mock-cats';
+import * as api from '../api/fetch-cat-image';
+import type { CatImage } from '../sources/types/cat-image';
+
+vi.mock('../api/fetch-cat-image', () => ({
+  fetchCatImage: vi.fn(),
+}));
 
 describe('Cat card', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   it('should render a cat card with link', () => {
     render(<CatCard cat={mockCats[0]} />);
+
     expect(
       screen.getByRole('link', { name: mockCats[0].name })
     ).toHaveAttribute('href', mockCats[0].wikipedia_url);
+  });
+
+  it('should fetch image and show in card if it exists', async () => {
+    const fakeCat = mockCats[1];
+    const fakeCatImg: CatImage = {
+      id: fakeCat.reference_image_id,
+      url: 'https://cat.com/british.jpg',
+      height: 700,
+      width: 700,
+    };
+    (api.fetchCatImage as ReturnType<typeof vi.fn>).mockResolvedValue(
+      fakeCatImg
+    );
+
+    render(<CatCard cat={fakeCat} />);
+
+    expect(api.fetchCatImage).toHaveBeenCalledWith(fakeCat.reference_image_id);
+    expect(
+      await screen.findByRole('img', { name: fakeCat.name })
+    ).toHaveAttribute('src', fakeCatImg.url);
+  });
+
+  it('does not show cat image if image fetch fails', async () => {
+    const fakeCat = mockCats[1];
+    (api.fetchCatImage as ReturnType<typeof vi.fn>).mockRejectedValue({
+      message: 'Not Found',
+    });
+
+    render(<CatCard cat={fakeCat} />);
+
+    expect(api.fetchCatImage).toHaveBeenCalledWith(fakeCat.reference_image_id);
+    expect(
+      await screen.findByRole('link', { name: fakeCat.name })
+    ).toHaveAttribute('href', fakeCat.wikipedia_url);
+    expect(
+      screen.queryByRole('img', { name: fakeCat.name })
+    ).not.toBeInTheDocument();
   });
 });
