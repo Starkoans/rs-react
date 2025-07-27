f92aa35a7dbe1b6b2069499cee65cc60a6c877db
Anastasiia <starkoans@gmail.com>
2025-07-27 23:01:50 +0500

    refactor: simulate error, styles

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,7 +2,6 @@ import { messages } from '@sources/messages';
 import { Link, Outlet } from 'react-router-dom';
 import styles from './layout.module.css';
 import { ROUTES } from '@/router';
-import { SimulateError } from '@/components/simulate-error';
 
 export const Layout = () => {
   return (
@@ -14,7 +13,6 @@ export const Layout = () => {
           </Link>
           <Link to={ROUTES.about}>{messages.links.about}</Link>
         </nav>
-        <SimulateError />
       </header>
       <Outlet />
     </>
