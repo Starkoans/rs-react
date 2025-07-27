f5ceaa6cdbb50f3611e59166bbb57a8b47830aab
Anastasiia <starkoans@gmail.com>
2025-07-27 22:59:59 +0500

    refactor: cat detail

--- src/components/cat-detail/cat-detail.tsx
+++ src/components/cat-detail/cat-detail.tsx
@@ -5,23 +5,27 @@ import type { Cat } from '@/sources/types/cat';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import styles from './cat-detail.module.css';
+import { fetchCatImage } from '@/api/fetch-cat-image';
 
-interface CatDetailProps {
-  catId: string;
-}
+export const CatDetail = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const catId = searchParams.get(URL_SEARCH_PARAMS.cat);
 
-export const CatDetail: React.FC<CatDetailProps> = ({ catId }) => {
   const [cat, setCat] = useState<Cat.Breed>();
+  const [imgUrl, setImgUrl] = useState<string>();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const getCat = async () => {
       try {
+        if (!catId) return;
         setIsLoading(true);
         setError(null);
-        const res = await fetchCatById(catId);
-        setCat(res);
+        const catInfo = await fetchCatById(catId);
+        const imgInfo = await fetchCatImage(catInfo.reference_image_id);
+        setCat(catInfo);
+        setImgUrl(imgInfo.url);
       } catch (error) {
         const err =
           error instanceof Error ? error.message : messages.errors.default;
@@ -30,10 +34,10 @@ export const CatDetail: React.FC<CatDetailProps> = ({ catId }) => {
         setIsLoading(false);
       }
     };
-    if (catId) getCat();
+
+    getCat();
   }, [catId]);
 
-  const [searchParams, setSearchParams] = useSearchParams();
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -51,23 +55,31 @@ export const CatDetail: React.FC<CatDetailProps> = ({ catId }) => {
 
   if (!catId) return null;
   if (error) return <div>{error}</div>;
-  if (!cat) return <div>{messages.noCatsFound}</div>;
   return (
-    <div
-      className={styles.backdrop}
-      onClick={onClose} // клик по фону закрывает
-    >
+    <div className={styles.backdrop} onClick={onClose}>
       <div
         className={`${styles.drawer} ${isVisible ? styles.drawerVisible : ''}`}
         onClick={(e) => e.stopPropagation()}
       >
+        <button className={styles.closeBtn} onClick={onClose}>
+          {messages.buttons.close}
+        </button>
         {isLoading ? (
-          <div>Loading...</div>
+          <h3>{messages.paragraphs.loading}</h3>
         ) : (
           <>
-            <button onClick={onClose}>close</button>
-            <p>{cat.id}</p>
-            <p>{cat.name}</p>
+            <div className={styles.imageWrapper}>
+              {imgUrl && (
+                <img src={imgUrl} alt={cat?.name} className={styles.image} />
+              )}
+            </div>
+            <h3>{cat?.name}</h3>
+            <p className={styles.temperament}>{cat?.temperament}</p>
+            <p>{cat?.description}</p>
+            <p>
+              {messages.paragraphs.breedFrom}
+              <b>{cat?.origin}</b>
+            </p>
           </>
         )}
       </div>
--- src/sources/messages.ts
+++ src/sources/messages.ts
@@ -1,6 +1,6 @@
 export const messages = {
   headers: {
-    appName: '≽^-⩊-^≼ Cat-a-log',
+    appName: 'Cat-a-log',
     notFound: 'Page not found.',
   },
   links: {
@@ -13,6 +13,7 @@ export const messages = {
     toMainPage: 'To main page',
     prev: '←',
     next: '→',
+    close: '✗',
   },
   input: {
     search: 'Search by breed name',
@@ -20,8 +21,14 @@ export const messages = {
   noCatsFound: 'No cats found for the given breed name.',
   paragraphs: {
     totalFound: 'Total cats found',
+    loading: 'Loading...',
     currentPage: 'page',
     of: 'of',
+    myGitHub: 'Author GitHub',
+    RSSLink: 'RS School React course',
+    aboutProject:
+      'is study project built during a frontend development course at',
+    breedFrom: 'This bred is from: ',
   },
   errors: {
     oops: 'Oops! Something went wrong.',
