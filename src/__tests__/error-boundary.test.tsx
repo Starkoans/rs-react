3468039406f760e3a0c2fd284e619ae39dab4cb9
Anastasiia <starkoans@gmail.com>
2025-07-27 23:04:03 +0500

    refactor: error boundary test

--- src/__tests__/error-boundary.test.tsx
+++ src/__tests__/error-boundary.test.tsx
@@ -1,23 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { ErrorBoundary } from '../components/error-boundary/error-boundary';
 import { messages } from '../sources/messages';
-import { SimulateError } from '../components/simulate-error';
-import userEvent from '@testing-library/user-event';
 
 describe('Error boundary', () => {
-  it('should catch error and show fallback UI when user clicked simulate error button', async () => {
+  it('should catch error and show fallback UI', async () => {
+    const SimulateError = () => {
+      throw new Error('oops');
+    };
+
     render(
       <ErrorBoundary>
         <SimulateError />
       </ErrorBoundary>
     );
-
-    const user = userEvent.setup();
-    const simulateErrorBtn = screen.getByRole('button', {
-      name: messages.buttons.simulateError,
-    });
-
-    await user.click(simulateErrorBtn);
 
     const errorMessage = screen.getByText(messages.errors.oops);
     const reloadBtn = screen.getByRole('button', {
