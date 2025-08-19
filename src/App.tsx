69c4eb742d622d7fa6d025bda6b3ba2b754443ee
Anastasiia <starkoans@gmail.com>
2025-08-19 22:31:38 +0500

    feat: modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import "./App.css";
+import { Modal } from "./components/modal";
+import { useState } from "react";
 
 function App() {
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-	return <>hello</>;
+	const onControlled = () => {
+		setIsModalOpen(true);
+	};
+
+	const onUncontrolled = () => {
+		setIsModalOpen(true);
+	};
+	return (
+		<>
+			<button onClick={onControlled}>Open controlled form</button>
+			<button onClick={onUncontrolled}>Open uncontrolled form</button>
+			<Modal open={isModalOpen} onClose={() => setIsModalOpen(false)} />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/modal.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, type FC, type ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+interface Props {
+	children?: ReactNode;
+	open?: boolean;
+	onClose?: () => void;
+}
+
+export const Modal: FC<Props> = ({ onClose, open, children }) => {
+	const ref = useRef<HTMLDialogElement>(null);
+
+	useEffect(() => {
+		const dialog = ref.current;
+		if (!dialog) return;
+
+		if (open) {
+			if (!dialog.open) {
+				dialog.showModal();
+			}
+		} else {
+			if (dialog.open) {
+				dialog.close();
+			}
+		}
+	}, [open]);
+
+	const handleOutsideClick = (e: React.MouseEvent<HTMLDialogElement>) => {
+		const dialog = ref.current;
+		if (!dialog) return;
+
+		const rect = dialog.getBoundingClientRect();
+		const isInDialog =
+			e.clientX >= rect.left &&
+			e.clientX <= rect.right &&
+			e.clientY >= rect.top &&
+			e.clientY <= rect.bottom;
+
+		if (!isInDialog) {
+			onClose?.();
+		}
+	};
+
+	if (!open) return null;
+	return createPortal(
+		<dialog ref={ref} onClose={onClose} onClick={handleOutsideClick}>
+			<div>
+				{children}
+				<button onClick={onClose}>Close</button>
+			</div>
+		</dialog>,
+		document.body
+	);
+};
